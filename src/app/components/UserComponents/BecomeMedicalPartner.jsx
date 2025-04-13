cc523a84bb9fdbf19f83e0c323eb05c3e7bded6c
samratcodes <samratcodess@gmail.com>
2025-04-13 14:05:27 +0545

    recreational booking and cancel

---
+++ src/app/components/UserComponents/BecomeMedicalPartner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const BecomeMedicalPartner = () => {
+  return (
+    <div>
+      <h1 className='text-green-600'>Become a Medical partner </h1>
+    </div>
+  )
+}
+
+export default BecomeMedicalPartner
